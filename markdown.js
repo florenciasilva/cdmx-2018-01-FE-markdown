2a530c9525dde6732446ee7320c5f81daa130ee8
Hensgrej <hensgrej@gmail.com>
2018-08-28 17:05:13 -0500

    Retorno de URLs en README.md

--- markdown.js
+++ markdown.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 
 fs.readFile('README.md', 'utf8', function(err, data) {  
     if (err) throw err;
+    console.log(data);
     let content = '';
     for(let i = 0; i < data.length; i++) {
         let allData = data[i];
         content += allData
+        
     }
-    if (content === 'Hola') {
-        console.log('!!!!!!!!!!!!!!!!')
+    let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+    let found = content.match(regex);
+
+    if (found) {
+        console.log(found)
     } else {
         console.log(':c')
     }
