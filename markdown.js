3751ab54e70a4ba5a6bb0ea9846fe9f53e78948a
Hensgrej <hensgrej@gmail.com>
2018-08-30 17:53:29 -0500

    Mostrando resultados mediante readline

--- markdown.js
+++ markdown.js
@@ -1,35 +1,57 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const fetch = require('node-fetch')
-        
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
 fs.readFile('README.md', 'utf8', function(err, data) {  
     if (err) throw err;
     let content = '';
     for(let i = 0; i < data.length; i++) {
         let allData = data[i];
-        content += allData        
+        content += allData
     }
-    const validate = () => {
-        let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-        let found = content.match(regex);
-        if (found) {
+    let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+    let found = content.match(regex);
+    console.log(found);
+    rl.question('¿Quieres verificar cada link? S/N ', (answer) => {
+        if(answer === 'S') {
+            let broken = 0;
+            let active = 0;
+            let total = 0;
             for (let i = 0; i < found.length; i++) {
                 let request = found[i]
-                //console.log(request);
             fetch(request).then((result) => {
                 let status = result.status
-                //console.log(status);
                 if (status === 200) {
-                    console.log('OK')
+                    console.log(found[i] + ' => ' +'OK ' + status)
+                    active[i] += active++
+                    total[i] += total++
                 } else {
-                    console.log('FAIL')
+                    console.log(found[i] + ' => ' +'FAIL ' + status)
+                    broken[i] += broken++
+                    total[i] += total++
                 }
                 return status
             }).catch((err) => {
                 console.log(err.message);
                 })
-            
-            }
+                };
+            setTimeout(() => {
+                rl.question('¿Quieres ver las estadisticas? S/N ', (answer2) => {
+                    if(answer2 === 'S') {
+                        console.log(broken + active + total);
+                        }
+                    });             
+                }, 3500)
         }
-    }
-validate();
+        rl.close();
+    });
 });
