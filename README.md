e8b6368e3037e393fb0650f6c793c59a62163427
Hensgrej <hensgrej@gmail.com>
2018-09-04 14:32:22 -0500

    Ordenando funciones

--- README.md
+++ README.md
@@ -20,8 +20,7 @@ algunas estadísticas.
 ![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
 
 ## Introducción
-
+[prueba](https://www.npmjsdks.com)
 [Node.js](https://nodejs.org/es/) es un entorno de ejecución para JavaScript
 construido con el [motor de JavaScript V8 de Chrome](https://developers.google.com/v8/).
 
-[prueba](https://www.npmjs.com/lskdfhldks)
--- markdown.js
+++ markdown.js
@@ -1,57 +1,47 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
 const fetch = require('node-fetch')
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-
-fs.readFile('README.md', 'utf8', function(err, data) {  
-    if (err) throw err;
-    let content = '';
-    for(let i = 0; i < data.length; i++) {
-        let allData = data[i];
-        content += allData
-    }
-    let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-    let found = content.match(regex);
-    console.log(found);
-    rl.question('¿Quieres verificar cada link? S/N ', (answer) => {
-        if(answer === 'S') {
-            let broken = 0;
-            let active = 0;
-            let total = 0;
-            for (let i = 0; i < found.length; i++) {
-                let request = found[i]
-            fetch(request).then((result) => {
-                let status = result.status
-                if (status === 200) {
-                    console.log(found[i] + ' => ' +'OK ' + status)
-                    active[i] += active++
-                    total[i] += total++
-                } else {
-                    console.log(found[i] + ' => ' +'FAIL ' + status)
-                    broken[i] += broken++
-                    total[i] += total++
-                }
-                return status
-            }).catch((err) => {
-                console.log(err.message);
-                })
-                };
-            setTimeout(() => {
-                rl.question('¿Quieres ver las estadisticas? S/N ', (answer2) => {
-                    if(answer2 === 'S') {
-                        console.log(broken + active + total);
-                        }
-                    });             
-                }, 3500)
+const readfile = (getURLs) => {
+fs.readFile('README.md', 'utf8', (err, data) => {  
+    if (err) {
+        console.log(err.message)
+        } else {
+        let content = '';
+        for(let i = 0; i < data.length; i++) {
+            let allData = data[i];
+            content += allData
+            }
+        getURLs(content);
         }
-        rl.close();
     });
-});
+}
+
+const getURLs = (content) => {
+let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+let found = content.match(regex);
+console.log(found);
+        let broken = 0;
+        let active = 0;
+        let total = 0;
+        for (let i = 0; i < found.length; i++) {
+            let request = found[i]
+        fetch(request).then((result) => {
+            let status = result.status
+            if (status === 200) {
+                console.log(found[i] + ' => ' +'OK ' + status)
+                active[i] += active++
+                total[i] += total++
+            } else {
+                console.log(found[i] + ' => ' +'FAIL ' + status)
+                broken[i] += broken++
+                total[i] += total++
+            }
+            return status
+        }).catch((err) => {
+            console.log(err.message);
+            })
+            };
+}
+
+readfile(getURLs);
