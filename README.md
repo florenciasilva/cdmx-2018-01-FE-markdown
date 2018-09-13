140dabdc5cf4e88247c118091f46b737c808d742
Hensgrej <hensgrej@gmail.com>
2018-09-13 17:13:22 -0500

    Refactorizando código, manteniendo funcionalidades

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
-# Markdown Links
+# Módulo md-links
 
-## Preámbulo
+md-links nace de la necesidad de mantener nuestros README actualizados y sin ningún link roto.
+Éste módulo recorre archivos .md, encontrando URLs y haciendo una petición HTTP/s para validar su estado. 
 
-[Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
-ligero muy popular entre developers. Es usado en muchísimas plataformas que
-manejan texto plano (GitHub, foros, blogs, ...), y es muy común
-encontrar varios archivos en ese formato en cualquier tipo de repositorio
-(empezando por el tradicional `README.md`).
+## Comandos
 
-Estos archivos `Markdown` normalmente contienen _links_ (vínculos/ligas) que
-muchas veces están rotos o ya no son válidos y eso perjudica mucho el valor de
-la información que se quiere compartir.
+#### En Linea de Comandos
+``` md-links ``` para encontrar URLs en archivos ```.md```
 
-Dentro de una comunidad de código abierto, nos han propuesto crear una
-herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos
-en formato `Markdown`, para verificar los links que contengan y reportar
-algunas estadísticas.
+```--validate``` para hacer la petición HTTP/s
 
-![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
+```--validate --stats ``` para hacer un conteo de cuantos links están activos y cuantos están rotos.
 
-## Introducción
-[prueba](https://www.npmjsdks.com)
-[Node.js](https://nodejs.org/es/) es un entorno de ejecución para JavaScript
-construido con el [motor de JavaScript V8 de Chrome](https://developers.google.com/v8/).
+```md-links``` devolverá en consola un arreglo de URLs. Al aplicarle ```--validate``` nos devolvera, por linea, la URL y su número de status. *Ejemplo*:
+> https://google.com 200
+
+> https://twitter.com 200
+
+
+## Instalación
+
+Ubicate en la carpeta del proyecto que quieres analizar y en consola escribe:  
+```npm install https://github.com/florenciasilva/cdmx-2018-01-FE-markdown.git ```
+
+Una vez que la instalación haya terminado, ya puedes utilizar los comandos ```md-links``` con sus opciones ```--validate``` y ```--stats```
 
--- markdown.js
+++ markdown.js
@@ -2,71 +2,58 @@
 
 const fs = require('fs');
 const fetch = require('node-fetch')
-
-const readfile = (getURLs) => {
-fs.readFile('README.md', 'utf8', (err, data) => {  
-    if (err) {
-        console.log(err.message)
-        } else {
-        let content = '';
-        for(let i = 0; i < data.length; i++) {
-            let allData = data[i];
-            content += allData
-            }
-        getURLs(content);
-        }
-    });
-}
-const counter = () => {
-let broken = 1;
-let active = 0;
-let total = 0;
-
-console.log('Broken' + ' ' + broken)
-}
-
-const getURLs = (content, requestHTTP) => {
-    let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-    let found = content.match(regex);
-    console.log(found);
-        for (let i = 0; i < found.length; i++) {
-            let request = found[i]
-            const requestHTTP = (request) => {
-                fetch(request).then((result) => {
-                    let status = result.status
-                    if (status === 200) {
-                        console.log(found[i] + ' => ' +'OK ' + status)
-                        active++
-                        total++
-                    } else {
-                        console.log(found[i] + ' => ' +'FAIL ' + status)
-                        broken++
-                        total++
-                    }
-                    return status
-                }).catch((err) => {
-                    console.log(err.message);
-                    })
-                    };
-                    requestHTTP(request)
-                } 
-};
-
-
 const program = require('commander');
 
 program
   .version('0.1.0')
   .option('validate, --validate', 'Validate all URLs from .md')
-  .option('status, --status', 'See Status of all URLs from .md')
+  .option('stats, --stats', 'See Status of all URLs from .md')
   .parse(process.argv);
 
-if (program.validate) {
-    readfile(getURLs);
-};
-if (program.status) {
-    counter();
+fs.readFile('README.md', 'utf8', (err, data) => {
+    if (err) throw err;
+    let regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+    let found = data.match(regex);
+    let urls = [];
+    
+    for(let i = 0; i < found.length; i++) {
+       urls.push(found[i]);
+    }
+    console.log(urls);
+    
+    if (program.validate) {
+        requestHTTP(urls);
+    };
+
+    return urls
+});
+
+const requestHTTP = (found) => {
+    let broken = 0;
+    let active = 0;
+    let total = 0;
+    for(let i = 0; i < found.length; i++) {
+        fetch(found[i]).then((response) => {
+    let status = response.status;
+    console.log(found[i] + ' ' + status);
+    if(status >= 200 && status <= 400) {
+        active++
+        total ++
+    } else {
+        broken++
+        total++
+    }
+    return status       
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }
+    if (program.validate && program.stats) {
+        setTimeout( () => {
+        console.log('Broken' + ' ' + broken + '\n' +
+                    'Active' + ' ' + active + '\n' +
+                    'Total'  + ' ' + total);
+        }, 5000)
+    };
 }
-/*if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);*/
+    
